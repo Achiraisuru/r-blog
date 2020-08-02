1bc6fc648495d474bf8f2fec7d217246afbdf709
Achiraisuru <achiraisuru@gmail.com>
2020-08-03 00:49:39 +0530

    routes created

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import Posts from './Posts/Posts';
+import {Route,NavLink} from 'react-router-dom';
+import NewPosts from './NewPost/NewPost';
+import Fullpost from './FullPost/FullPost';
+
+import './Blog.css';
+
+class Blog extends Component {
+  
+
+  
+
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/" 
+                            exact
+                            activeClassName="my-active"
+                            activeStyle={{
+                                color:'#fa923f',
+                                textDecoration:'underline'
+                            }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname:'/new-post',
+                                hash:'#submit',
+                                search:'?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+               {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
+
+               <Route path="/" exact component={Posts}/>
+               
+               <Route path="/new-post" component={NewPosts}/>
+               <Route path="/posts/:id" exact component={Fullpost}/>
+            </div>
+        );
+    }
+}
+
+
+export default Blog;
