3bffd3082f2992fc4f8605aacc077cc6e639901d
Achiraisuru <achiraisuru@gmail.com>
2020-08-05 20:33:35 +0530

    lazy routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import Posts from './Posts/Posts';
 import {Route,NavLink,Switch} from 'react-router-dom';
-import NewPosts from './NewPost/NewPost';
-
-
+// import NewPosts from './NewPost/NewPost';
 import './Blog.css';
 
+
+
+import asyncComponet from '../../hoc/asyncComponents';
+const AsyncNewPost  = asyncComponet(() => {
+    return import('./NewPost/NewPost');
+});
+
+
+
 class Blog extends Component {
-  
+  state={
+      auth:true
+  }
 
   
 
     render () {
+
         return (
             <div className="Blog">
                 <header>
@@ -37,8 +47,11 @@ class Blog extends Component {
                {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
               
                 <Switch>
-                <Route path="/new-post" component={NewPosts}/>
+                {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                 <Route path="/posts" component={Posts}/>
+                <Route render={() => <h1>Not found</h1>}/>
+                {/* <Redirect from="/" to="/posts"/> */}
+                {/* <Route path="/" component={Posts}/> */}
                
                </Switch>
             </div>
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,11 +8,13 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted:false
         
     }
 
     componentDidMount(){
+        //IF unauth => this.props.history.replace('/posts');
         console.log(this.props);
     }
 
@@ -24,12 +27,21 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                // this.setState({submitted:true});
+                this.props.history.replace('/posts');
             });
     }
 
     render () {
+
+        let redirect = null;
+        if(this.state.submitted){
+            redirect = <Redirect to="/posts"/>
+        }
+
         return (
             <div className="NewPost">
+               {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
